c449bfa32559094e3194bf2ec4e5708065151033
ayeshkadike <ayeshkadike@gmail.com>
2025-05-24 18:23:38 -0400

    fixed bugs

--- DQNAgent.py
+++ DQNAgent.py
@@ -11,33 +11,33 @@ class DQNAgent:
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=lr)
         self.loss_fn = tf.keras.losses.MeanSquaredError()
 
-        def select_action(self, state, epsilon):
-            if np.random.rand() < epsilon:
-                return np.random.randint(self.num_actions)  # Random action (explore)
-            
-            state = np.expand_dims(state, axis=0)  # Add batch dimension
-            q_values = self.model.predict(state, verbose=0)
-            return np.argmax(q_values[0])  # Best action (exploit)
+    def select_action(self, state, epsilon):
+        if np.random.rand() < epsilon:
+            return np.random.randint(self.num_actions)  # Random action (explore)
         
-        def train(self, states, actions, rewards, next_states, dones):
-            # Compute target Q-values using target network
-            target_q = self.target_model.predict(next_states, verbose=0)
-            max_q_next = np.max(target_q, axis=1)
-            target_values = rewards + (1 - dones) * self.gamma * max_q_next
-
-            with tf.GradientTape() as tape:
-                q_values = self.model(states, training=True)
-                # Gather predicted Q-values for the actions taken
-                indices = np.arange(q_values.shape[0])
-                selected_q = tf.gather(q_values, actions[:, None], batch_dims=1)
-                loss = self.loss_fn(target_values, selected_q[:, 0])
-
-            grads = tape.gradient(loss, self.model.trainable_variables)
-            self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
-
-            return loss.numpy()
+        state = np.expand_dims(state, axis=0)  # Add batch dimension
+        q_values = self.model.predict(state, verbose=0)
+        return np.argmax(q_values[0])  # Best action (exploit)
         
-        def update_target_model(self):
-            self.target_model.set_weights(self.model.get_weights())
+    def train(self, states, actions, rewards, next_states, dones):
+        # Compute target Q-values using target network
+        target_q = self.target_model.predict(next_states, verbose=0)
+        max_q_next = np.max(target_q, axis=1)
+        target_values = rewards + (1 - dones) * self.gamma * max_q_next
+
+        with tf.GradientTape() as tape:
+            q_values = self.model(states, training=True)
+            # Gather predicted Q-values for the actions taken
+            indices = np.arange(q_values.shape[0])
+            selected_q = tf.gather(q_values, actions[:, None], batch_dims=1)
+            loss = self.loss_fn(target_values, selected_q[:, 0])
+
+        grads = tape.gradient(loss, self.model.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.model.trainable_variables))
+
+        return loss.numpy()
+        
+    def update_target_model(self):
+        self.target_model.set_weights(self.model.get_weights())
 
 
